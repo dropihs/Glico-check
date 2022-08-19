b1fd988a2398bc0b7f3f739b1935485b0409e6fb
Pedro <pedrosoaresty@gmail.com>
2022-08-19 17:49:38 -0300

    layout home page

--- glicoCheck/app/src/main/java/com/app/glicoCheck/activity/HomeActivity.java
+++ glicoCheck/app/src/main/java/com/app/glicoCheck/activity/HomeActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.app.glicoCheck.R;
 import com.app.glicoCheck.Util.ConfiguracaoBd;
@@ -12,12 +15,21 @@ import com.google.firebase.auth.FirebaseAuth;
 public class HomeActivity extends AppCompatActivity {
 
     private FirebaseAuth auth;
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         auth = ConfiguracaoBd.Firebaseautenticacao();
+
     }
+
+
+
 
     public void deslogar(View view){
         try{
@@ -27,5 +39,9 @@ public class HomeActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+
+
+
+
 
 }
