3198e28e0c211c86e25b1b360044c0a597c1a56e
pedro <pedrosoaresty@gmail.com>
2022-10-13 18:47:50 -0300

    versao instavel2

--- glicoCheck/app/src/main/java/com/app/glicoCheck/Util/DAOUsuario.java
+++ glicoCheck/app/src/main/java/com/app/glicoCheck/Util/DAOUsuario.java
@@ -1,29 +1,69 @@
 package com.app.glicoCheck.Util;
+import android.provider.ContactsContract;
+
+import androidx.annotation.NonNull;
+
 import com.app.glicoCheck.model.GlicoseUser;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class DAOUsuario {
-    private DatabaseReference databaseReference;
-    FirebaseDatabase db = FirebaseDatabase.getInstance();
+    private DatabaseReference mReferenceGlicose;
+    private FirebaseDatabase mDatabase;
 
-    private List<GlicoseUser> glicoseUserList = new ArrayList<>();
+
+    private final List<GlicoseUser> glicoseUserList = new ArrayList<>();
+
+    public interface DataStatus{
+        void DataIsLoaded(List<GlicoseUser> glicoseUserList, List<String> keys);
+        void DataIsInserted();
+        void DataIsUpdated();
+        void DataIsDeleted();
+    }
 
 
     public DAOUsuario(){
-        FirebaseDatabase db = FirebaseDatabase.getInstance();
-        databaseReference = db.getReference(GlicoseUser.class.getSimpleName());
-
-
+        FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+        mReferenceGlicose = mDatabase.getReference("glicose");
     }
 
     public Task<Void> add(GlicoseUser user){
         //if (user == null) //throw exception
-        return databaseReference.push().setValue(user);
+        return mReferenceGlicose.push().setValue(user);
     }
+
+
+
+    public void readGlicose(final DataStatus dataStatus){
+        mReferenceGlicose.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                glicoseUserList.clear();
+                List<String> keys = new ArrayList<>();
+                for(DataSnapshot keyNode: snapshot.getChildren()){
+                    keys.add(keyNode.getKey());
+                    GlicoseUser glicoseUser = keyNode.getValue(GlicoseUser.class);
+                    glicoseUserList.add(glicoseUser);
+                }
+                dataStatus.DataIsLoaded(glicoseUserList,keys);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+
 
 }
--- glicoCheck/app/src/main/java/com/app/glicoCheck/activity/HistoricoGlicActivity.java
+++ glicoCheck/app/src/main/java/com/app/glicoCheck/activity/HistoricoGlicActivity.java
@@ -1,19 +1,52 @@
 package com.app.glicoCheck.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.app.glicoCheck.R;
+import com.app.glicoCheck.Util.DAOUsuario;
+import com.app.glicoCheck.model.GlicoseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.List;
 
 public class HistoricoGlicActivity extends AppCompatActivity {
+
+    FirebaseDatabase db = FirebaseDatabase.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historico_glic);
+        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView_Glicose);
+
+        new DAOUsuario().readGlicose(new DAOUsuario.DataStatus() {
+            @Override
+            public void DataIsLoaded(List<GlicoseUser> glicoseUserList, List<String> keys) {
+                new RecyclerView_config().setConfig(mRecyclerView, HistoricoGlicActivity.this,glicoseUserList,keys);
+            }
+
+            @Override
+            public void DataIsInserted() {
+
+            }
+
+            @Override
+            public void DataIsUpdated() {
+
+            }
+
+            @Override
+            public void DataIsDeleted() {
+
+            }
+        });
+
+
     }
     public void voltarHistGlic(View v){
         Intent i = new Intent(this, regGlicoseActivity.class);
