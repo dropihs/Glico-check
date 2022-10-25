833e6c81555fdb6bce12513b33c10a29fec0419c
pedro <pedrosoaresty@gmail.com>
2022-10-25 17:33:49 -0300

    alarm test2

--- glicoCheck/app/src/main/java/com/app/glicoCheck/activity/AlarmReceiver.java
+++ glicoCheck/app/src/main/java/com/app/glicoCheck/activity/AlarmReceiver.java
@@ -1,42 +1,37 @@
 package com.app.glicoCheck.activity;
 
-import android.app.Notification;
+
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.os.Build;
-import android.os.VibrationEffect;
-import android.os.Vibrator;
-import android.widget.Toast;
 
-import androidx.annotation.RequiresApi;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import com.app.glicoCheck.R;
 
-public class AlarmReceiver extends BroadcastReceiver
-{
-    @RequiresApi(api = Build.VERSION_CODES.Q)
+public class    AlarmReceiver extends BroadcastReceiver {
     @Override
-    public void onReceive(Context context, Intent intent)
-    {
-        //we will use vibrator first
-        Vibrator vibrator = (Vibrator)context.getSystemService(context.VIBRATOR_SERVICE);
-        vibrator.vibrate(4000);
+    public void onReceive(Context context, Intent intent) {
 
-        Toast.makeText(context, "Horario da insulina!", Toast.LENGTH_LONG).show();
-        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
-        if (alarmUri == null)
-        {
-            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        }
-        Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
-        ringtone.play();
+        Intent i = new Intent(context,DestinationActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context,0,i,0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context,"foxandroid")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Foxandroid Alarm Manager")
+                .setContentText("Subscribe for android related content")
+                .setAutoCancel(true)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent);
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
+        notificationManagerCompat.notify(123,builder.build());
+
 
     }
 }
---
+++ glicoCheck/app/src/main/java/com/app/glicoCheck/activity/DestinationActivity.java
@@ -0,0 +1,16 @@
+package com.app.glicoCheck.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.glicoCheck.R;
+
+public class DestinationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_destination);
+    }
+}
